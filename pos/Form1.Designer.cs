287fdcf879ff25b3552a4184ab8715907fa4eda9
Lilian Joune Sanders <a22311012@uthermosillo.edu.mx>
2023-05-12 17:27:28 -0700

    inicio de la practica de punto de venta
    Signed-off-by: Lilian Joune Sanders <a22311012@uthermosillo.edu.mx>

---
+++ pos/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace pos
+{
+    partial class titulo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.dataGridproductos = new System.Windows.Forms.DataGridView();
+            this.Desarrollado = new System.Windows.Forms.Label();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.time = new System.Windows.Forms.Timer(this.components);
+            this.FechaHora = new System.Windows.Forms.Label();
+            this.LblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridproductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.Location = new System.Drawing.Point(344, 49);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(177, 28);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.Text = "Punto de Venta";
+            // 
+            // dataGridproductos
+            // 
+            this.dataGridproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridproductos.Location = new System.Drawing.Point(321, 211);
+            this.dataGridproductos.Name = "dataGridproductos";
+            this.dataGridproductos.RowHeadersVisible = false;
+            this.dataGridproductos.Size = new System.Drawing.Size(184, 95);
+            this.dataGridproductos.TabIndex = 1;
+            // 
+            // Desarrollado
+            // 
+            this.Desarrollado.AutoSize = true;
+            this.Desarrollado.Location = new System.Drawing.Point(343, 95);
+            this.Desarrollado.Name = "Desarrollado";
+            this.Desarrollado.Size = new System.Drawing.Size(140, 13);
+            this.Desarrollado.TabIndex = 3;
+            this.Desarrollado.Text = "Desarrollado por: Lilibombon";
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::pos.Properties.Resources.yo;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(696, 12);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(69, 47);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogo.TabIndex = 4;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcodigo.Location = new System.Drawing.Point(74, 267);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(170, 35);
+            this.txtcodigo.TabIndex = 5;
+            this.txtcodigo.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.txtcodigo_PreviewKeyDown);
+            // 
+            // time
+            // 
+            this.time.Enabled = true;
+            this.time.Interval = 1000;
+            this.time.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // FechaHora
+            // 
+            this.FechaHora.AutoSize = true;
+            this.FechaHora.Location = new System.Drawing.Point(387, 126);
+            this.FechaHora.Name = "FechaHora";
+            this.FechaHora.Size = new System.Drawing.Size(60, 13);
+            this.FechaHora.TabIndex = 6;
+            this.FechaHora.Text = "FechaHora";
+            // 
+            // LblTotal
+            // 
+            this.LblTotal.AutoSize = true;
+            this.LblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTotal.Location = new System.Drawing.Point(605, 135);
+            this.LblTotal.Name = "LblTotal";
+            this.LblTotal.Size = new System.Drawing.Size(83, 16);
+            this.LblTotal.TabIndex = 7;
+            this.LblTotal.Text = "Total $0.00";
+            // 
+            // titulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblTotal);
+            this.Controls.Add(this.FechaHora);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.Desarrollado);
+            this.Controls.Add(this.dataGridproductos);
+            this.Controls.Add(this.Nombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "titulo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Punto de Venta Lilibombon";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.titulo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridproductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.DataGridView dataGridproductos;
+        private System.Windows.Forms.Label Desarrollado;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Timer time;
+        private System.Windows.Forms.Label FechaHora;
+        private System.Windows.Forms.Label LblTotal;
+    }
+}
+
---
+++ pos/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pos
+{
+    public partial class titulo : Form
+    {
+        public titulo()
+        {
+            InitializeComponent();
+        }
+
+        private void titulo_Load(object sender, EventArgs e)
+        {
+            FechaHora.Text = DateTime.Now.ToLongTimeString() + " " + DateTime.Now.ToLongDateString();
+            ///localizar el lable (titulo) de la pagina a la mitad
+            Nombre.Location = new Point((this.Width /2)-(Nombre.Width / 2), 0);
+            ///centrar el  lable Desarrollado por y ponerlo debajo  del titulo
+            Desarrollado.Location = new Point((this.Width / 2)-(Desarrollado.Width / 2), Nombre.Height);
+            ///centrada debajo del Desarrollado por...
+            FechaHora.Location = new Point((this.Width / 2)-(FechaHora.Width / 2), (Nombre.Height + Desarrollado.Height));
+            ///ubicar el dataGrid debajo de los titulos, 
+            dataGridproductos.Location = new Point((10), (Nombre.Height + Desarrollado.Height + FechaHora.Height));
+            dataGridproductos.Width = this.Width - 40;
+            dataGridproductos.Height=(this.Height/ 4 )* 3;
+            pictureBoxLogo.Location = new Point((this.Width-pictureBoxLogo.Width), 0);
+            txtcodigo.Location = new Point((10), this.Height-txtcodigo.Height);
+            txtcodigo.Width = this.Width - 40;/// segun el nivel del dataGrid
+            dataGridproductos.Columns.Add("Cantidad", "Cantidad");
+            dataGridproductos.Columns[0].HeaderCell.Style.Font = new Font("Arial", 20F);
+            dataGridproductos.Columns[0].Width = (dataGridproductos.Width *10) / 100;
+            dataGridproductos.Columns.Add("Nombre", "Nombre");
+            dataGridproductos.Columns[1].HeaderCell.Style.Font = new Font("Arial", 20F);
+            dataGridproductos.Columns[1].Width = (dataGridproductos.Width * 50) /100;
+            dataGridproductos.Columns.Add("Precio", "Precio");
+            dataGridproductos.Columns[2].HeaderCell.Style.Font = new Font("Arial", 20F);
+            dataGridproductos.Columns[2].Width = (dataGridproductos.Width * 20)/100;
+            dataGridproductos.Columns.Add("Total", "Total");
+            dataGridproductos.Columns[3].HeaderCell.Style.Font = new Font("Arial", 20F);
+            dataGridproductos.Columns[3].Width = (dataGridproductos.Width * 20) /100;
+            dataGridproductos.ReadOnly=true;
+            Font fuente = new Font("Arial", 20);
+            LblTotal.Font = fuente;
+            int posicion = (this.Height - (Nombre.Height + Desarrollado.Height + FechaHora.Height + dataGridproductos.Height + txtcodigo.Height));
+            LblTotal.Location = new Point (10 + dataGridproductos.Columns[0].Width + dataGridproductos.Columns[1].Width + dataGridproductos.Columns[2].Width , (Nombre.Height + Desarrollado.Height + FechaHora.Height + dataGridproductos.Height) + posicion/4);
+            txtcodigo.TabIndex = 0;
+        }
+        
+        ///Personlizar el lable de hora y fecha segun la hora y fecha del sistema.
+        private void time_Tick(object sender, EventArgs e)
+        {
+            FechaHora.Text=DateTime.Now.ToLongTimeString() + " " + DateTime.Now.ToLongDateString();
+        }
+
+        private void txtcodigo_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                MessageBox.Show("se encontro el articulo" + txtcodigo.Text);
+                txtcodigo.Clear();
+            }
+        }
+    }
+}
